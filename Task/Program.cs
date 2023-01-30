1fc2381bcd710418c625065e049831cfbd5dab9b
Pavel <andros88pa@mail.ru>
2023-01-30 10:47:11 +0700

    Добавил код программы

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿void ArrayMetod(string[] array1, string[] array2)
+{
+    int Maxlenght = 3;
+    int count = 0;
+
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= Maxlenght)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    Array.Resize(ref array2, count);
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", array2)}]");
+}
+Console.Clear();
+Console.Write("Введите кол-во строк массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[n];
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write("Введите строку массива: ");
+        array1[i] = Console.ReadLine();
+    }
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array1)}]");
+string[] array2 = new string[array1.Length];
+ArrayMetod(array1, array2);
